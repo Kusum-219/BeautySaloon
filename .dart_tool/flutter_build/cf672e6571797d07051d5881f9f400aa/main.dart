9fd95157cd81e5a9f398dc1551dfae1eb5227b68
Kusum-219 <negikusum52000@gmail.com>
2021-01-02 21:23:18 +0530

    Second commit changes done in ist page font size of text

---
+++ .dart_tool/flutter_build/cf672e6571797d07051d5881f9f400aa/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter_app_assignment/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
